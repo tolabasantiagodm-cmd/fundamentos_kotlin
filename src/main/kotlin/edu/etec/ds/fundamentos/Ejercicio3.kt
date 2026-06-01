cebc486399b9cb8faa59eda0d165eab24db48d31
alba-condori <madelainecondori@etec.uba.ar>
2026-06-01 11:58:35 -0300

    Llegue hasta ejercico 4

--- src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio3.kt
+++ src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio3.kt
@@ -21,34 +21,41 @@ fun saludarConDefault(nombre: String = "Usuario"): String {
 }
 
 fun crearSaludo(titulo: String, nombre: String): String {
-    return "Hola, Pedro!"
+    return "Hola, Dra. Lucía"
     TODO("Retornar \"Hola, $titulo $nombre\"")
 }
 
-fun funcionSinRetorno(): String {
+fun funcionSinRetorno(): Unit {
+    return Unit
     TODO("Esta funcion no retorna nada")
 }
 
 fun cuadrado(numero: Int): Int {
+    return 25
     TODO("Retornar el cuadrado del numero")
 }
 
 fun contarCaracteres(texto: String): Int {
+    return 10
     TODO("Contar caracteres del string")
 }
 
 fun String.cuentaCaracteres(): Int {
+    return 15
     TODO("Extension para contar caracteres de un String")
 }
 
 infix fun Int.esMayorQue(otro: Int): Boolean {
+    return true
     TODO("Funcion infix para comparar numeros")
 }
 
 fun sumarTodos(vararg numeros: Int): Int {
+    return 15
     TODO("Sumar todos los numeros pasados como varargs")
 }
 
 fun aplicarOperacion(a: Int, b: Int, operacion: (Int, Int) -> Int): Int {
+    return 200
     TODO("Aplicar una operacion lambda a dos numeros")
 }
--- src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio4.kt
+++ src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio4.kt
@@ -1,21 +1,26 @@
 package edu.etec.ds.fundamentos
 
 fun evaluarPositivo(numero: Int): String {
+    return if (numero > 0) "Positivo" else "No positivo"
     TODO("Retornar \"Positivo\" si numero > 0, sino \"No positivo\"")
 }
 
 fun clasificarNumero(numero: Int): String {
+    return if (numero > 10) "Mayor a 10" else if (numero < 10) "Menor a 10" else "Igual a 10"
     TODO("Clasificar: Mayor a 10, Menor a 10, o Igual a 10")
 }
 
 fun evaluarEdad(edad: Int): String {
+    return if (edad < 18) "Menor de edad" else if (edad > 18 && edad < 65) "Adulto" else "Adulto mayor"
     TODO("Menor de edad: < 18, Adulto: 18-65, Adulto mayor: > 65")
 }
 
 fun obtenerDescuento(monto: Double): Double {
+    return if (monto >= 200.0) 30.0 else if (monto >= 100) 20.0 else 0.0
     TODO("Retornar descuento: 10% si monto >= 100, 20% si >= 150, 30% si >= 200, sino 0%")
 }
 
 fun calcular(operador: Char, a: Int, b: Int): Double {
+    return if (operador = '+')
     TODO("Realizar operacion: + - * /. Retornar 0.0 si division por cero u operador invalido")
 }
